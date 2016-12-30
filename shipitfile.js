ab702339571b91894a9d43df3ea697ea30b0c599
Jeremy Bunting <buntingj@gmail.com>
2016-12-30 12:11:43 -0800

    adding GA tags, setting up deploy script

---
+++ shipitfile.js
@@ -0,0 +1,20 @@
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '.shipit-temp',
+            dirToCopy:'public',
+            deployTo: '/var/www/ctcoppersmiths.com/public_html',
+            repositoryUrl: 'https://github.com/qbunt/ctcoppersmiths.com.git',
+            ignores: ['.git', 'node_modules'],
+            keepReleases: 2,
+            deleteOnRollback: false,
+            key: '~/.ssh/id_rsa',
+            shallowClone: true
+        },
+        production: {
+            servers: 'qbunt@vermouth'
+        }
+    });
+};
